9665cded3449102026919d76abeef10a81cc5310
Donnie Moore <donnie.m@gvocom.com>
2014-07-10 14:32:07 -0500

    created grain: number of outstanding package updates

---
+++ package.updates.py
@@ -0,0 +1,10 @@
+# @author Donald Ray Moore Jr (dmoore@suspectedterrorist.org)
+# @purpose salt grain to count redhat packages
+
+import os
+
+def package_update_count():
+  return {'package updates': os.popen("yum check-update | grep -v "^ * " | wc -l").readline().rstrip()}
+
+if __name__ == '__main__':
+  package_update_count()
