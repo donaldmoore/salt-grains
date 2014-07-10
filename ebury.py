6dcde9e6a200053a646ba27b55ce44eaadef4b6c
Donnie Moore <donnie.m@gvocom.com>
2014-07-10 11:22:58 -0500

    added a constant for libkeyutil library size and some comments

--- ebury.py
+++ ebury.py
@@ -3,10 +3,16 @@
 
 import os
 
+# real library file size of libkeyutils.so.1.3
+REAL_LIBRARY_SIZE = 10192
+
 def ebury_detect():
+  # look first in /lib64, if not found it's in /lib
   root_path = 'lib64' if os.path.isfile('/lib64/libkeyutils.so.1.3') else 'lib'
+  # grab the file size of the potentially infected library  
   size = os.path.getsize('/%(root_path)s/libkeyutils.so.1.3' % locals())
-  is_ebury = 'True' if size > 32768 else 'False'
+  
+  is_ebury = 'True' if size > REAL_LIBRARY_SIZE else 'False'
   return {'ebury': is_ebury }
 
 if __name__ == '__main__':
